5312200c11be1f6662814c9daff5a6356d74d816
Awawa <69086569+awawa-dev@users.noreply.github.com>
2026-09-07 22:54:09 +0200

    Update: qt 6.8.4, ccache 4.14, Debian Forky (#15)
    * Update: qt_6.8.4 LTS LGPL, ccache 4.14
    
    * Update Debian scripts
    
    * Update Debian scripts (2)
    
    * Update Debian scripts (3)
    
    * Update Debian scripts (4)
    
    * Update Debian scripts (5)
    
    * Update Debian scripts (6)
    
    * Update Debian scripts (7)
    
    * Update Debian scripts (8)
    
    * Update Debian scripts (9)
    
    * Update Debian scripts (10)
    
    * Update Debian scripts (11)
    
    * Update Debian scripts (12)
    
    * Update Fedora
    
    * test
    
    * Update build.sh
    
    * Update build.sh
    
    * Update
    
    * Update buildDebian
    
    * Update buildDebian
    
    * Update buildDebian
    
    * Update buildDebian
    
    * Update push.yml
    
    * Update buildDebian
    
    * Update buildDebian
    
    * Update buildDebian
    
    * Update buildDebian

--- build.sh
+++ build.sh
@@ -4,22 +4,12 @@
 #                BUILD QT                  #
 ############################################
 
-qt_version="6.8.3"
+qt_version="6.8.4"
 
-required_version="3.27.7"
-current_version=$(cmake --version | head -n1 | awk '{print $3}' | sed 's/[^0-9.].*$//')
-if [ "$(printf '%s\n' "$current_version" "$required_version" | sort -V | head -n1)" != "$required_version" ]; then
-  echo "CMake version $current_version is older than $required_version"
-  build_option_qt="-no-sbom"
-  build_option_qt_serial="-DQT_GENERATE_SBOM=OFF"  
-else
-  echo "CMake version is $current_version. SBOM is supported."
-fi
-
-git clone --branch v${qt_version} https://github.com/qt/qtbase.git qt_lts
+git clone --depth 1 --branch v${qt_version}-lts-lgpl https://code.qt.io/qt/qtbase.git qt_lts
 mkdir qt_build
 cd qt_build
-../qt_lts/configure -prefix /usr -bindir /usr/qt_${qt_version}_bin -headerdir /usr/qt_${qt_version}_include -hostdatadir /usr/qt_${qt_version}_host -archdatadir /usr/qt_${qt_version} -datadir /usr/qt_${qt_version} -submodules qtbase,qtnetwork ${build_option_qt} -no-dbus -no-gui -no-widgets -no-sql-sqlite -no-icu -skip qtsql -skip qtxml -nomake tests -nomake examples
+../qt_lts/configure -prefix /usr -bindir /usr/qt_${qt_version}_bin -headerdir /usr/qt_${qt_version}_include -hostdatadir /usr/qt_${qt_version}_host -archdatadir /usr/qt_${qt_version} -datadir /usr/qt_${qt_version} -no-dbus -no-gui -no-widgets -no-icu -no-feature-sql -no-feature-xml -nomake tests -nomake examples
 if [ "$?" -ne "0" ]; then
   echo "Qt configuration failed"
   exit 1
@@ -35,8 +25,8 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 
-rm -r * .*
-git clone --branch v${qt_version} https://github.com/qt/qtserialport.git qtserialport
+rm -rf * .[!.]* && ls -la
+git clone --depth 1 --branch v${qt_version}-lts-lgpl https://code.qt.io/qt/qtserialport.git qtserialport
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ${build_option_qt_serial} ./qtserialport
 if [ "$?" -ne "0" ]; then
   echo "Qt serial configuration failed"
@@ -69,7 +59,7 @@ fi
 #              BUILD CCACHE                #
 ############################################
 
-ccache_version="4.13.2"
+ccache_version="4.14"
 
 mkdir ccache
 cd ccache
